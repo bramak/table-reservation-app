411e1a48cda08a59c082db4462ffadd86a35cf61
AnushaChattopadhyay <anusha.chattopadhyay@cesltd.com>
2020-09-04 04:01:37 +0530

    display changes made

--- utils/detailsscripts.js
+++ utils/detailsscripts.js
@@ -40,6 +40,22 @@
     evt.currentTarget.className += " active";
   }
 
+  //sidetabfunction
+  function openpage2(evt, pagename) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(pagename).style.display = "block";
+    evt.currentTarget.className += " active";
+    tablinksclose();
+  }
+
 
   // create an element
   const createNode = (elem) => {
