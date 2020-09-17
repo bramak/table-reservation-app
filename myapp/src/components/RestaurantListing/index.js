3eab5c267abe614f018600adc0c4319925a1c76b
AnushaChattopadhyay <anusha.chattopadhyay@cesltd.com>
2020-09-17 22:35:09 +0530

    unfinished tags removed

--- myapp/src/components/RestaurantListing/index.js
+++ myapp/src/components/RestaurantListing/index.js
@@ -7,30 +7,12 @@ console.log(Styles);
 class Restarauntlisting extends React.Component {
   ViewFilters = () => {};
 
-  openNav = () => {
-    document.getElementById("mySidenav").style.width = "250px";
-  };
-
-  closeNav = () => {
-    document.getElementById("mySidenav").style.width = "0";
-  };
   render() {
     return (
       <div>
-        <div id="mySidenav" class="sidenav">
-          <a href="javascript:void(0)" class="closebtn" onclick={this.closeNav}>
-            &times;
-          </a>
-          <a href="#">About</a>
-          <a href="#">Services</a>
-          <a href="#">Clients</a>
-          <a href="#">Contact</a>
-        </div>
-        <span onclick={this.openNav}>&#9776; open</span>
         <Tabs>
           <div label="Restaraunts" className={Styles.flexContainer}>
             <List onClickBusiness={this.props.onClickBusiness} />
-            <button onClick={this.ViewFilters}>value</button>
           </div>
           <div label="About Us">info</div>
         </Tabs>
